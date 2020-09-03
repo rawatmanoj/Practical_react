da105e7147137ec7407a09d581a3d586e7c58a24
rawatmanoj <42403468+rawatmanoj@users.noreply.github.com>
2020-09-03 21:11:29 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { useState, useRef, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0);
   const countRef = useRef(0);
 
   countRef.current = count;
 
-  useEffect(() => {
+  function handleAlertClick() {
     setTimeout(() => {
-      alert("You clicked on: " + countRef.current);
+      alert("you clicked " + countRef.current);
     }, 3000);
-  }, []);
+  }
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     alert("You clicked on: " + countRef.current);
+  //   }, 3000);
+  // }, []);
 
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
-      {/* <button onClick={handleAlertClick}>Show alert</button> */}
+      <button onClick={handleAlertClick}>Show alert</button>
     </div>
   );
 }
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
